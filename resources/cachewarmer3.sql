5c6c05076a5710226ebfb14d8587625ccfab64f2
Evan Wills <evan.wills@acu.edu.au>
2014-04-03 14:09:00 +1100

    updated cachewarmer3.sql. Added columns to URLs table for whether or not HTTP and/or HTTPS urls are cached at all plus added a number of indexes to speed up searching URLs.

--- resources/cachewarmer3.sql
+++ resources/cachewarmer3.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 02, 2014 at 12:06 PM
+-- Generation Time: Apr 03, 2014 at 08:06 AM
 -- Server version: 5.5.35
 -- PHP Version: 5.4.4-14+deb7u7
 
@@ -42,13 +42,21 @@ CREATE TABLE IF NOT EXISTS `places` (
 DROP TABLE IF EXISTS `urls`;
 CREATE TABLE IF NOT EXISTS `urls` (
   `url_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `url_url` text NOT NULL,
-  `url_http` tinyint(3) unsigned NOT NULL DEFAULT '1',
-  `url_https` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `url_http_cache_expiry` datetime DEFAULT NULL,
-  `url_https_cache_expiry` datetime DEFAULT NULL,
-  PRIMARY KEY (`url_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `url_url` text NOT NULL COMMENT 'string reversed URL with http(s):// removed',
+  `url_http` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT 'Whether or not the URL works for plain HTTP',
+  `url_http_cached` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'whether or not the HTTP url is cached',
+  `url_http_cache_expires` datetime DEFAULT NULL COMMENT 'cache expiry time for HTTP URL',
+  `url_https` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Whether or not the URL works for HTTPS',
+  `url_https_cached` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'whether or not the HTTPS url is cached',
+  `url_https_cache_expires` datetime DEFAULT NULL COMMENT 'cache expiry time for HTTPS URL',
+  PRIMARY KEY (`url_id`),
+  KEY `IND_url_http` (`url_http`),
+  KEY `IND_url_http_cached` (`url_http_nocache`),
+  KEY `IND_url_https` (`url_https`),
+  KEY `IND_url_https_cached` (`url_https_nocache`),
+  KEY `IND__url_https__url_https_cached` (`url_https`,`url_https_nocache`),
+  KEY `IND__url_http__url_http_cached` (`url_http`,`url_http_nocache`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
