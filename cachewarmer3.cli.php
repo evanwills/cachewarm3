8810775b2772f49ea66c5a59191fac91a872eccd
Evan Wills <evan.wills@acu.edu.au>
2014-04-03 16:19:41 +1100

    updated cachewarmer3.cli.php added much of the code to make the cachewarmer work. Still a lot to go to get it fully functional

--- cachewarmer3.cli.php
+++ cachewarmer3.cli.php
@@ -59,6 +59,125 @@ if(!function_exists('debug'))
 // END: debug include
 // ==================================================================
 
-require_once($cls.'curl_get/curl_get.class.php');
+require_once($cls.'curl_get/curl_get_simple.class.php');
+require_once($cls.'db_class/db_abstract.class.php');
+require_once($cls.'db_class/db_mysql.class.php');
+require_once('classes/curl_get_cache.class.php');
+require_once('config_db.php');
+
+$source_list = 'http://www.acu.edu.au/admin/warm_up/list_all/';
+#$source_list = 'http://www.acu.edu.au/admin/warm_up/list_all/_nocache';
+
+$db = new db_mysql( array( 'host' => $host , 'username' => $db_user , 'password' => $db_pass , 'database' => $db_name ) );
+
+
+$serverOffset = new DateTime( 'now' , new DateTimeZone( date_default_timezone_get() ) );
+$GMT_offset = $serverOffset->getOffset();
+unset($serverOffset);
+
+$curl = new curl_get_cache();
+$mem = round( ( memory_get_usage() / 1024 ) / 1024 ,3 );
+debug( $mem );
+
+while( $mem < 50 )
+{
+	$now = '"'.$db->escape(gmdate('Y-m-d H:i:s')).'"';
+	$now_time = gmmktime();
+	$sql = '
+SELECT	 `url_id` AS `id`
+	,REVERSE(`url_url`) AS `url`
+	,`url_url_sub` AS `sub`
+	,`url_http_cached` AS `http_cached`
+	,`url_http_cache_expires` AS `http_expires`
+	,`url_https_cached` AS `https_cached`
+	,`url_https_cache_expires` AS `https_expires`
+FROM	`urls`
+WHERE
+(
+	`url_http` = 1
+AND	`url_http_cached` = 1
+AND	`url_http_cache_expires` < '.$now.'
+)
+OR
+(
+	`url_https` = 1
+AND	`url_https_cached` = 1
+AND	`url_https_cache_expires` < '.$now.'
+)
+LIMIT 0,100
+';
+	$result = $this->fetch($sql);
+	if( $result !== null )
+	{
+		for( $a = 0 ; $a < count($result) ; $a += 1 )
+		{
+			$sql = '';
+			$sep = ' ';
+			if( $result[$a]['http_cached'] && strtotime($result[$a]['http']) < $now_time )
+			{
+				// Warm the cache for the HTTP version of the URL
+				$http = $curl->warm_url( 'http://'.$result['url'] );
+
+				if( strtotime($http['expires']) > strtotime($result[$a]['http_expires']) )
+				{
+					$sql .= "\t$sep`url_http` = {$http['is_valid']}\n\t,`url_http_cached` = {$http['is_cached']}\n\t,`url_http_cache_expires` = '"
+					     .$db->escape(date('Y-m-d H:i:s',$http['expires']))."'";
+					$sep = ',';
+				}
+				else
+				{
+					debug($result[$a],$http,'Cache was not warmed for HTTPS');
+				}
+				unset($http);
+			}
+
+			if( $result[$a]['https_cached'] && strtotime($result[$a]['http_expires']) < $now_time )
+			{
+				// Warm the cache for the HTTPS version of the URL
+				$https = $curl->warm_url( 'https://'.$result['url'] );
+				
+				if( strtotime($https['expires']) > strtotime($result[$a]['https_expires']) )
+				{
+					$sql .= "\t$sep`url_https` = {$https['is_valid']}\n\t,`url_https_cached` = {$https['is_cached']}\n\t,`url_https_cache_expires` = '"
+					     .$db->escape(date('Y-m-d H:i:s',$https['expires']))."'";
+				}
+				else
+				{
+					debug($result[$a],$https,'Cache was not warmed for HTTPS');
+				}
+				unset($https);
+			}
+
+			if( $sql != '' )
+			{
+				$sql = "\nUPDATE\t`urls`\nSET{$sql}\nWHERE\t`url_id` = {$result[$a]['id']}";
+				$db->query($sql);
+			}
+		}
+	}
+	else
+	{
+		$sql = '
+SELECT	 `url_http_cache_expires` AS `http_expires`
+	,`url_https_cache_expires` AS `https_expires`
+FROM	`urls`
+WHERE
+(
+	`url_http` = 1
+AND	`url_http_cached` = 1
+AND	`url_http_cache_expires` >= '.$now.'
+)
+OR
+(
+	`url_https` = 1
+AND	`url_https_cached` = 1
+AND	`url_https_cache_expires` >= '.$now.'
+)
+ORDER BY `http_expires` DESC
+,ORDER BY `https_expires` DESC
+LIMIT 0,1
+';
+	}
+}
 
 
