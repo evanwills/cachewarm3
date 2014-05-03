3329a3b53ee8f8172d9f209616b761fccf098a78
Evan Wills <evan.wills@acu.edu.au>
2014-05-04 06:44:07 +1000

    updated resources/cachewarmer3.sql added comments to all fields. added url_sub field to speed up matching URLs. Changed *_cached to *_is_cached to help with understanding purpose

--- resources/cachewarmer3.sql
+++ resources/cachewarmer3.sql
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `places`;
 CREATE TABLE IF NOT EXISTS `places` (
-  `place_name` varchar(32) NOT NULL,
-  `place__url_id` int(10) unsigned NOT NULL,
+  `place_name` varchar(32) NOT NULL COMMENT 'Name of the search using the URLs list',
+  `place__url_id` int(10) unsigned NOT NULL COMMENT 'ID of the last URL checked',
   UNIQUE KEY `place_name` (`place_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -42,21 +42,23 @@ CREATE TABLE IF NOT EXISTS `places` (
 DROP TABLE IF EXISTS `urls`;
 CREATE TABLE IF NOT EXISTS `urls` (
   `url_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `url_url` text NOT NULL COMMENT 'string reversed URL with http(s):// removed',
-  `url_http` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT 'Whether or not the URL works for plain HTTP',
-  `url_http_cached` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'whether or not the HTTP url is cached',
-  `url_http_cache_expires` datetime DEFAULT NULL COMMENT 'cache expiry time for HTTP URL',
-  `url_https` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Whether or not the URL works for HTTPS',
-  `url_https_cached` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'whether or not the HTTPS url is cached',
-  `url_https_cache_expires` datetime DEFAULT NULL COMMENT 'cache expiry time for HTTPS URL',
-  PRIMARY KEY (`url_id`),
-  KEY `IND_url_http` (`url_http`),
-  KEY `IND_url_http_cached` (`url_http_nocache`),
-  KEY `IND_url_https` (`url_https`),
-  KEY `IND_url_https_cached` (`url_https_nocache`),
-  KEY `IND__url_https__url_https_cached` (`url_https`,`url_https_nocache`),
-  KEY `IND__url_http__url_http_cached` (`url_http`,`url_http_nocache`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `url_url` text NOT NULL COMMENT 'String reversed URL with http(s):// removed',
+  `url_url_sub` CHAR(2) NOT NULL COMMENT 'Last two chars of the URL',
+  `url_http_ok` tinyint(1) unsigned DEFAULT NULL COMMENT 'Whether or not the URL works for plain HTTP',
+  `url_http_is_cached` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether or not the HTTP url is cached',
+  `url_http_cache_expires` datetime DEFAULT NULL COMMENT 'Cache expiry time for HTTP URL',
+  `url_https_ok` tinyint(1) unsigned DEFAULT NULL COMMENT 'Whether or not the URL works for HTTPS',
+  `url_https_is_cached` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether or not the HTTPS url is cached',
+  `url_https_cache_expires` datetime DEFAULT NULL COMMENT 'Cache expiry time for HTTPS URL',
+  PRIMARY KEY			(`url_id`),
+  KEY `IND_url_sub`		(`url_url_sub`),
+  KEY `IND_url_http`		(`url_http`),
+  KEY `IND_url_http_is_cached`	(`url_http_is_cached`),
+  KEY `IND_url_https`		(`url_https`),
+  KEY `IND_url_https_is_cached`	(`url_https_is_cached`),
+  KEY `IND__url_http__url_http_is_cached`	(`url_http`,`url_http_is_cached`)
+  KEY `IND__url_https__url_https_is_cached`	(`url_https`,`url_https_is_cached`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
