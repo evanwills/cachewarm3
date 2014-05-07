f44a1f2ecb35f2afc68ee21dd2ae5940b5aee287
Evan Wills <evan.wills@acu.edu.au>
2014-05-07 14:52:39 +1000

    updated resources/cachewarmer3.sql fixed numerous SQL syntax errors. renamed  table to  and renamed all it's fields accordingly.

--- resources/cachewarmer3.sql
+++ resources/cachewarmer3.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `urls` (
   `url__url_status_id` tinyint(3) unsigned NOT NULL DEFAULT 1 COMMENT 'Foreign key to the url_status table. The status of the URL',
   PRIMARY KEY			(`url_id`),
   KEY `IND_url_sub`		(`url_url_sub`),
-  KEY `IND_url_delete`		(`url_delete`),
+  KEY `IND_url__url_status_id`		(`url__url_status_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores unique URLs';
 
 -- --------------------------------------------------------
@@ -48,33 +48,33 @@ CREATE TABLE IF NOT EXISTS `_url_status` (
   `url_status_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `url_status_name` varchar(8) NOT NULL COMMENT 'The status of the URL (e.g. "new", "good", "delete")',
   PRIMARY KEY				(`url_status_id`),
-  UNIQUE KEY `IND_url_status_name`	(`url_status_name`),
+  UNIQUE KEY `IND_url_status_name`	(`url_status_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Normalised table for URL statuses';
 
-INSERT INTO `cachewarmer3`.`url_status` ( `status_id` , `status_name` )
-VALUES ( 1 , 'new' ) , ( 2 , 'good' ) , ( 3 , 'stale') , ( 4 , 'delete');
+INSERT INTO `cachewarmer3`.`_url_status` ( `url_status_id` , `url_status_name` )
+VALUES ( 1 , 'new' ) , ( 2 , 'good' ) , ( 3 , 'stale') , ( 4 , 'bad' ) , ( 5 , 'delete');
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `urls`
 --
 
-DROP TABLE IF EXISTS `protocol`;
-CREATE TABLE IF NOT EXISTS `protocol` (
-  `protocol__url_id` int(10) unsigned NOT NULL COMMENT 'Foreign key to the urls table. The unique ID of the URL',
-  `protocol_https` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether or not this relates to a HTTPS version of a URL',
-  `protocol_ok` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether or not the URL works for this protocol',
-  `protocol_is_cached` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether or not the URL for this protocol is cached',
-  `protocol_cache_expires` datetime DEFAULT NULL COMMENT 'Cache expiry time for the URL for this protocol',
-  PRIMARY KEY					(`protocol_id`),
-  UNIQUE KEY `UNI_protocol_url_id__protocol_https` (`protocol__url_id`,`protocol_https`),
-  KEY `IND_protocol_https`			(`protocol_https`),
-  KEY `IND_protocol_ok`				(`protocol_ok`),
-  KEY `IND_protocol_is_cached`			(`protocol_is_cached`),
-  KEY `IND__protocol_https__protocol_ok`	(`protocol_https`,`protocol_ok`)
-  KEY `IND__protocol_https__protocol_is_cached`	(`protocol_https`,`protocol_is_cached`)
-  KEY `IND__protocol_ok__protocol_is_cached`	(`protocol_ok`,`protocol_is_cached`)
-  KEY `IND__protocol_https__protocol_ok__protocol__is_cached`	(`protocol_https`,`protocol_ok`,`protocol_https_is_cached`),
+DROP TABLE IF EXISTS `url_by_protocol`;
+CREATE TABLE IF NOT EXISTS `url_by_protocol` (
+  `url_by_protocol__url_id` int(10) unsigned NOT NULL COMMENT 'Foreign key to the urls table. The unique ID of the URL',
+  `url_by_protocol_https` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether or not this relates to a HTTPS version of a URL',
+  `url_by_protocol_ok` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether or not the URL works for this url_by_protocol',
+  `url_by_protocol_is_cached` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether or not the URL for this url_by_protocol is cached',
+  `url_by_protocol_cache_expires` datetime DEFAULT NULL COMMENT 'Cache expiry time for the URL for this url_by_protocol',
+  UNIQUE KEY `UNI_url_by_protocol_url_id__url_by_protocol_https` (`url_by_protocol__url_id`,`url_by_protocol_https`),
+  KEY `IND_protocol_url_id`			(`url_by_protocol__url_id`),
+  KEY `IND_protocol_https`			(`url_by_protocol_https`),
+  KEY `IND_protocol_ok`				(`url_by_protocol_ok`),
+  KEY `IND_protocol_is_cached`			(`url_by_protocol_is_cached`),
+  KEY `IND__protocol_https__protocol_ok`	(`url_by_protocol_https`,`url_by_protocol_ok`),
+  KEY `IND__protocol_https__protocol_is_cached`	(`url_by_protocol_https`,`url_by_protocol_is_cached`),
+  KEY `IND__protocol_ok__protocol_is_cached`	(`url_by_protocol_ok`,`url_by_protocol_is_cached`),
+  KEY `IND__protocol_https__protocol_ok__protocol_is_cached`	(`url_by_protocol_https`,`url_by_protocol_ok`,`url_by_protocol_is_cached`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Contains all the data relating to the URL via a given protocl';
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
