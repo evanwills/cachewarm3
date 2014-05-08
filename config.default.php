3e5633fc1a9445cda5aa2b8309ceb4a12f2c70e2
Evan Wills <evan.wills@acu.edu.au>
2014-05-08 11:13:13 +1000

    updated config.default.php to make variable values generic. updated .gitignore to exclude config.php from git

--- config.default.php
+++ config.default.php
@@ -4,8 +4,7 @@
  * @ver string $source_list URL for page listing all the URLs whose
  *	cache should be added to the database
  */
-$source_list = 'http://www.acu.edu.au/admin/warm_up/list_all/';
-// $source_list = 'http://www.acu.edu.au/admin/warm_up/list_all/_nocache';
+$source_list = 'http://university.edu.au/list_urls_to_warm';
 
 /**
  * @var string $local_cache_path absolute path to where the local
