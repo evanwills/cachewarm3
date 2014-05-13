3abab7acf11c5a829409d7ba34d8dbd3961dcb6c
Evan Wills <evan.wills@acu.edu.au>
2014-05-13 10:45:15 +1000

    updated classes/cache_warm.class.php. Added lots of code comments.

--- classes/cache_warm.class.php
+++ classes/cache_warm.class.php
@@ -4,7 +4,6 @@ class cache_warm
 {
 	private $db = null;
 	private $curl = null;
-	private $GMT_offset = 0;
 	private $get_urls_count = 100;
 	private $limit_start = 0;
 	private $_order_by = ';
@@ -12,43 +11,91 @@ ORDER BY `urls`.`url_domain_priority` DESC
 	,`urls`.`url_depth` ASC
 	,`url_by_protocol`.`url_by_protocol_cache_expires` DESC';
 
-
+/**
+ * @method __construct()
+ *
+ * @param right_db $db database connection object
+ *
+ * @param curl_get_cache $curl cURL object to get the contents of URLs
+ *
+ * @param integer $limit_start When warming the URLs, the warming is
+ *	  done in batches. If you are running multiple instances of
+ *	  the warming script. $limit_start represents the order number
+ *	  of the instance so that there is no overlap in URLs that are
+ *	  warmed.
+ *
+ * @param string $priority_order the order in which to prioritise
+ *	  which URLs get warmed first
+ */
 	public function __construct( right_db $db , curl_get_cache $curl , $limit_start = 0 , $priority_order = 'domain_depth_expires')
 	{
+		// set the db object
 		$this->db = $db;
+
+		// cache any normalised tables in the DB to minimise the need for
+		/// table joins
 		$this->db->cache_normalised();
+
+		// set the cURL object
 		$this->curl = $curl;
 
-		$serverOffset = new DateTime( 'now' , new DateTimeZone( date_default_timezone_get() ) );
-		$this->GMT_offset = $serverOffset->getOffset();
-
+		// set limit start
 		if( is_int($limit_start) && $limit_start > 0 )
 		{
 			$this->limit_start = ( $limit_start * $this->get_urls_count );
 		}
+
+		// set the order_by priority. (Note this defines a static string used
+		// in a select statement.)
 		$this->set_order_by('domain_depth_expires');
 	}
 
-	public function update_url_list( $source_url , $priority_domains = array() )
+/**
+ * @method update_url_list() takes a URL that points to a page that
+ *	   lists all the URLs you want warmed (one URL per line) and
+ *	   adds any new URLs to the database
+ *
+ * @param string $source_url the URL for the page listing URLs.
+ *
+ * @param array $priority_sites list of sites (listed from most
+ *	  important to least important) by which to prioritise
+ *	  warming order. This is primarily for when you don't have
+ *	  enough resources on the this side to warm all URLs before
+ *	  their cache expires.
+ *
+ * @return boolean TRUE if any URLs were added or updated.
+ *	   FALSE otherwise
+ */
+	public function update_url_list( $source_url , $priority_sites = array() )
 	{
 		if( !$this->curl->valid_url($source_url) )
 		{
 			// throw
 			return false;
 		}
+
+		// get the URLs and put them into an array.
 		$url_list = explode("\n",$this->curl->get_content($source_url));
 
-		$priority_domains = $this->_make_priority_domains_usable( $priority_domains );
+		// make the list of priority_sites more usable by this method
+		$priority_sites = $this->_make_priority_sites_usable( $priority_sites );
 
-		$unprioritised = count($priority_domains);
+		// unprioritised sites get the lowest priority.
+		$unprioritised = count($priority_sites);
+
+		$output = false;
 
 		for( $a = 0 ; $a < count($url_list) ; $a += 1 )
 		{
 			$url_list[$a] = trim($url_list[$a]);
+
+			// validate the URL and get metadata about it
 			$url_bits = $this->curl->get_url_parts($url_list[$a]);
 			if( $url_bits != false )
 			{
 				$url = $url_bits['domain'].$url_bits['path'].$url_bits['file'];
+
+				// strip the protocol and leading slashes (it's redundant data in the DB)
 				if( $url_bits['protocol'] == 'https' )
 				{
 					$url_list[$a] = substr($url_list[$a],8);
@@ -57,13 +104,15 @@ ORDER BY `urls`.`url_domain_priority` DESC
 				{
 					$url_list[$a] = substr($url_list[$a],7);
 				}
+
+				// work out the site priority of the URL
 				$priority = false;
 				for( $b = 0 ; $b < $unprioritised ; $b += 1 )
 				{
-					if( substr( $url_list[$a] , 0 , $priority_domains[$b]['chars'] ) == $priority_domains[$b]['url'] )
+					if( substr( $url_list[$a] , 0 , $priority_sites[$b]['chars'] ) == $priority_sites[$b]['url'] )
 					{
 						$priority = $b;
-						if( $priority_domains[$b]['stop'] )
+						if( $priority_sites[$b]['stop'] )
 						{
 							break;
 						}
@@ -73,13 +122,29 @@ ORDER BY `urls`.`url_domain_priority` DESC
 				{
 					$priority = $unprioritised;
 				}
+
+				// work out how deep within the site the URL points to
 				$depth = substr_count($url_bits['path'],'/');
+
+				// to speed up searching, we reverse the URL on the premis
+				// that, as the domain is included in the URL, and the domain
+				// is likely to be consistant across a large number of URLs
+				// then having to search the domain needlessly adds time to
+				// the search where as the reversed URL is much more random
+				// and so the search will progress quicker 
 				$e_lru = $this->db->escape(strrev($url_list[$a]));
+
+				// to further help improve search speed we add the last two
+				// characters of the URL to it's own indexed column
 				$e_lr = $this->db->escape(substr(strrev($url_list[$a]),0,2));
-				$sql = 'SELECT `url_id` AS `id` , `url_domain_priority` AS `priority`  FROM `urls` WHERE `url_url_sub` = "'.$e_lr.'" AND `url_url` = "'.$e_lru.'"';
-				$result = $this->db->fetch_1_row($sql);
+
+				// See if the current URL is already listed in the DB
+				$result = $this->db->fetch_1_row(
+					'SELECT `url_id` AS `id` , `url_domain_priority` AS `priority`  FROM `urls` WHERE `url_url_sub` = "'.$e_lr.'" AND `url_url` = "'.$e_lru.'"'
+				);
 				if( $result === null )
 				{
+					// OK not listed. Let's add it to the DB
 					$sql = '
 INSERT INTO `urls`
 (
@@ -96,31 +161,34 @@ VALUES
 	,'.$priority.'
 )';
 					$this->db->query($sql);
-
+					$output = true;
 				}
 				elseif( $result['priority'] != $priority )
 				{
+					// Yep, it's aready in the DB but the priority has changed.
 					$this->db->query('UPDATE `urls` SET `url_domain_priority` = '.$priority.' WHERE `url_id` = '.$result['id']);
+					$output = true;
 				}
 			}
 		}
+		return $output;
 	}
 
 /**
- * @method _make_priority_domains_usable() takes an indexed array of
+ * @method _make_priority_sites_usable() takes an indexed array of
  *	   strings and builds a two dimensional array where the
  *	   second level provides meta info to help matching.
  *
- * @param array $input_priority_domains list of domains and or
+ * @param array $input_priority_sites list of domains and or
  *	  subsites that have priority
  *
  * @return array two dimensional array where the first level is an
  *	   index and the second level contains mata info about the
  *	   domain
  */
-	private function _make_priority_domains_usable( $input_priority_domains )
+	private function _make_priority_sites_usable( $input_priority_sites )
 	{
-		if( !is_array($input_priority_domains) )
+		if( !is_array($input_priority_sites) )
 		{
 			// throw
 			return array();
@@ -129,7 +197,7 @@ VALUES
 		{
 			$output_priorities_domains = array();
 			$tmp_test = array();
-			foreach( $input_priority_domains as $worthless_key => $value )
+			foreach( $input_priority_sites as $worthless_key => $value )
 			{
 				if( is_string($value) )
 				{
@@ -148,10 +216,11 @@ VALUES
 								// the domain which this site is a child of has been listed.
 								// change its stop status to fals so it doesn't block giving
 								// this site a lower priority than the parent domain
-								$output_priority_domains[$tmp_found]['stop'] = false;
+								$output_priority_sites[$tmp_found]['stop'] = false;
 							}
 						}
-						$output_priority_domains[] = array( 'chars' => $chars , 'url' => $value , 'stop' => true );
+						// Add the URL and metadata to the list of priority sites.
+						$output_priority_sites[] = array( 'chars' => $chars , 'url' => $value , 'stop' => true );
 						$tmp_test[] = $value;
 					}
 					// else  discard empty strings
@@ -159,13 +228,20 @@ VALUES
 				// else discard non strings
 
 			}
-			return $output_priority_domains;
+			return $output_priority_sites;
 		}
 	}
 
-
+/**
+ * @method check_new_urls() finds any URLs in the DB marked as new
+ *	   and grabs their headers to see if the URLs is good, if the
+ *	   page is cached and, if so, when the cache expires
+ *
+ * @return $output TRUE if there were new URLs to check. False otherrwise 
+ */
 	public function check_new_urls()
 	{
+		$output = false;
 		$sql = '
 SELECT	 `url_id` AS `id`
 	,REVERSE(`url_url`) AS `url`
@@ -243,12 +319,14 @@ VALUES
 						$this->db->query($url_good_sql.' )');
 						$url_good_sql = $url_good_sql_start;
 						$url_good_sep = '';
+						$output = true;
 					}
 					if( $url_bad_sep != '' )
 					{
 						$this->db->query($url_bad_sql.' )');
 						$url_bad_sql = $url_bad_sql_start;
 						$url_bad_sep = '';
+						$output = true;
 					}
 					$b = 0;
 				}
@@ -256,16 +334,20 @@ VALUES
 			if( $cache_sep != '' )
 			{
 				$this->db->query($cache_sql);
+				$output = true;
 			}
 			if( $url_good_sep != '' )
 			{
 				$this->db->query($url_good_sql.' )');
+				$output = true;
 			}
 			if( $url_bad_sep != '' )
 			{
 				$this->db->query($url_bad_sql.' )');
+				$output = true;
 			}
 		}
+		return $output;
 	}
 
 /**
@@ -296,10 +378,16 @@ VALUES
  */
 	public function get_urls_to_warm( $limit_start = true )
 	{
+		// get current GMT date/time
 		$gmt = gmdate('Y-m-d H:i:s');
+
+		// make current GMT date/time OK for use in SQL
 		$now = '"'.$this->db->escape($gmt).'"';
 		$now_time = strtotime($gmt);
 
+		// check to see if we need to get the first lot of
+		// URLs or if we can go to another page of URLs that
+		// need to be warmed
 		if( $limit_start === false )
 		{
 			$limit_start = 0;
@@ -309,6 +397,7 @@ VALUES
 			$limit_start = $this->limit_start;
 		}
 
+		// Select the URLs to be warmed.
 		$sql = '
 SELECT	 `urls`.`url_id` AS `id`
 	,REVERSE(`urls`.`url_url`) AS `url`
@@ -329,6 +418,7 @@ LIMIT '.$limit_start.','.$this->get_urls_count;
 			$c = count($result);
 			for( $a = 0 ; $a < $c ; $a += 1 )
 			{
+				// Add the protocol to the URL
 				if( $result[$a]['https'] == 1 )
 				{
 					$result[$a]['url'] = "https://{$result[$a]['url']}";
@@ -337,7 +427,9 @@ LIMIT '.$limit_start.','.$this->get_urls_count;
 				{
 					$result[$a]['url'] = "http://{$result[$a]['url']}";
 				}
+				// Add the GMT now time to the array for this URL
 				$result[$a]['now'] = $gmt;
+				// Add the difference between the $gmt now time and the cache expiration time
 				$result[$a]['age'] = ( $now_time - strtotime($result[$a]['expires']) );
 				$output[] = $result[$a];
 				unset($result[$a]);
@@ -346,10 +438,13 @@ LIMIT '.$limit_start.','.$this->get_urls_count;
 		}
 		elseif( $this->limit_start > 0 )
 		{
+			// OK there were no URLs in the page we selected, lets try from the top.
 			return $this->get_urls_to_warm( false );
 		}
 		else
 		{
+			// OK there were no URLs at all to warm (This is great!!!)
+			// Let's report how long it is before the next URL expires.
 			$sql = '
 SELECT	`url_by_protocol_cache_expires` AS `expires`
 FROM	`url_by_protocol`
@@ -361,9 +456,11 @@ LIMIT 0,1';
 			$next_expires = $this->db->fetch_1($sql);
 			if( $next_expires !== null )
 			{
+				// return the number of seconds till the cache expires for the next URL
 				return strtotime($next_expires) - $now_time;
 			}
 		}
+		// That's bad. Couldn't do anything... This should not happen.
 		return false;
 	}
 
@@ -451,6 +548,11 @@ LIMIT 0,1';
 	}
 
 
+/**
+ * @method get_uncached_urls() get a list of URL's that are not cached
+ *
+ * @param $start
+ */
 	public function get_uncached_urls($start = 0 )
 	{
 		if( !is_int($start) || $start < 100 )
@@ -476,6 +578,24 @@ AND	`url_by_protocol`.`url_by_protocol_is_cached` = 0';
 		return false;
 	}
 
+/**
+ * @method warm_url() attempts to warm the cache of an individual URL
+ *
+ * NOTE: If a URL could not be downloaded, it checks to see if a URL
+ *	 with the alternate protocol was successfully downloaded. If
+ *	 not it updates the entry for the URL to mark it's status as
+ *	 bad. Another script at a later stage will attempt to
+ *	 download the URL again. If that fails, the URL will be
+ *	 marked for deletion. One final attempt will be made download
+ *	 the URLs marked for deletion before the URL is finally
+ *	 deleted from the DB.
+ *
+ * @param array $url_info containing the URL and some metadata about
+ *	  that URL
+ *
+ * @return boolean TRUE if the URL was successfully downloaded. FALSE
+ *	   otherwise.
+ */
 	public function warm_url( $url_info )
 	{
 		if( !is_array($url_info) || !isset($url_info['url']) || !isset($url_info['sub']) || !isset($url_info['http']) || !isset($url_info['https']) )
@@ -528,6 +648,16 @@ AND	`url_by_protocol`.`url_by_protocol_https` = {$url_info['https']}";
 		$this->db->query($sql);
 
 		return $output;
+	}
+
+	public function check_bad_urls()
+	{
+		// TODO
+	}
+
+	public function check_delete_urls()
+	{
+		// TODO
 	}
 
 	public function set_get_urls_count( $count )
