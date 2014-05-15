68c27ecedcaba05abddee6943bedcc403914d2a5
Evan Wills <evan.wills@acu.edu.au>
2014-05-15 17:41:20 +1000

    updated classes/throttle.class.php removed object property $_throttle and all references to it in the code as it was redundant

--- classes/throttle.class.php
+++ classes/throttle.class.php
@@ -11,11 +11,12 @@ class throttle
 
 	public function __construct( $rate )
 	{
+		debug($rate);sleep(1);
 		if( is_numeric($rate) && $rate > 0 )
 		{
 			$this->_throttle_rate = ( 1 / $rate );
 			$this->_throttle_method = '_do_throttle';
-			$this->_throttle_time = microtime(true);
+			$this->_throttle_time = microtime(true);debug($this);
 		}
 	}
 
@@ -29,12 +30,13 @@ class throttle
 	
 	private function _do_throttle()
 	{
+		$now = microtime(true);
 		$time_span = microtime(true) - $this->_throttle_time;
-		if( $time_span < $this->throttle_time )
+		if( $time_span < $this->_throttle_rate )
 		{
-			$sleep_time = ( ( $this->throttle_time - $time_span ) / 1000000 );
+			$sleep_time = round( ( $this->_throttle_rate - $time_span ) * 1000000 );
 			usleep($sleep_time);
 		}
-		$this->throttle_time = microtime(true);
+		$this->_throttle_time = microtime(true);
 	}
 }
