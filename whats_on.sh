5ee7a049526d41b41822792e466019e3001c64c1
Evan Wills <evan.wills@acu.edu.au>
2014-05-24 11:32:23 +1000

    updated whats_on.sh added lots of stuff.

--- whats_on.sh
+++ whats_on.sh
@@ -1,3 +1,29 @@
 #! /bin/sh
+
+# ===============================================
+#
+# This script is used as a shortcut to find out what's going on with
+# cachwarmer3.cli.php on the system.
+#
+
+# ===============================================
+# @var string $cron_name the name of the unix user
+#	running the cron job that calls
+#	cachewarm_auto-restart.cron.sh 
+cron_user='evan'
+
+echo;
+echo;
+top -u "$cron_user";
+echo;
 echo $(date);
-ps auxh |grep 'cachewarm\|url' |grep -v 'grep'
+echo;
+ps aux |grep 'cachewarm\|url' |grep -v 'grep'
+echo;
+echo;
+echo;
+
+log_file='log/cachewarm_auto-restart.log';
+if [ -f $log_file ]
+then	tail $log_file;
+fi
